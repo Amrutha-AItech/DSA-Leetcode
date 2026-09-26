0660f908240e9e01939d5386efe6c9f01b06e144
Amrutha BM <amruthabm018@gmail.com>
2026-09-26 18:04:32 +0530

    Time: 10 ms (36.95%), Space: 22.2 MB (86.79%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int m, n;
+
+    void dfs(vector<vector<int>>& heights,
+             vector<vector<bool>>& visited,
+             int r, int c) {
+
+        visited[r][c] = true;
+
+        int dr[] = {1, -1, 0, 0};
+        int dc[] = {0, 0, 1, -1};
+
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+
+            if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+                continue;
+
+            if (visited[nr][nc])
+                continue;
+
+            // Reverse the water-flow direction:
+            // move from lower/equal height to higher/equal height.
+            if (heights[nr][nc] < heights[r][c])
+                continue;
+
+            dfs(heights, visited, nr, nc);
+        }
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        m = heights.size();
+        n = heights[0].size();
+
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
+
+        // Pacific: top row + left column
+        for (int c = 0; c < n; c++)
+            dfs(heights, pacific, 0, c);
+
+        for (int r = 0; r < m; r++)
+            dfs(heights, pacific, r, 0);
+
+        // Atlantic: bottom row + right column
+        for (int c = 0; c < n; c++)
+            dfs(heights, atlantic, m - 1, c);
+
+        for (int r = 0; r < m; r++)
+            dfs(heights, atlantic, r, n - 1);
+
+        vector<vector<int>> ans;
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (pacific[r][c] && atlantic[r][c])
+                    ans.push_back({r, c});
+            }
+        }
+
+        return ans;
+    }
+};
